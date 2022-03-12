c91b194443d94d4e282e54eee0367a87162dec73
unknown <felixurlaub@googlemail.com>
2022-03-12 11:08:15 +0100

    lines to int vec added

--- adventofcode/src/main.rs
+++ adventofcode/src/main.rs
@@ -1,10 +1,4 @@
 use util;
 
 fn main() {
-    let num = 10;
-    println!(
-        "Hello, world! {} plus one is {}!",
-        num,
-        util::add_one(num)
-    );
 }
--- util/src/lib.rs
+++ util/src/lib.rs
@@ -3,40 +3,46 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-// fn main() {
-//     // File hosts must exist in current path before this produces output
-//     if let Ok(lines) = read_lines("./hosts") {
-//         // Consumes the iterator, returns an (Optional) String
-//         for line in lines {
-//             if let Ok(ip) = line {
-//                 println!("{}", ip);
-//             }
-//         }
-//     }
-// }
-
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-pub fn read_file() -> i32 {
-    return 3;
+
+pub fn lines_to_int(file_content: io::Result<io::Lines<io::BufReader<File>>>) -> Vec<i32> {
+    let mut converted = Vec::new();
+    if let Ok(lines) = file_content{
+        for line in lines {
+            if let Ok(line) = line {
+                converted.push(line.parse().unwrap());
+            }
+        }
+    }
+    return converted;
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_reads_lines() {
-        if let Ok(file_content) = super::read_lines("../data/sample") {
+        if let Ok(file_content) = super::read_lines("././data/sample") {
             for line in file_content {
                 if let Ok(line) = line {
                    assert_eq!(line, "1");
                 }
             }
         }
- 
-        
     }
+    #[test]   
+    fn it_converts_read_lines_to_int() {
+            let file_content= super::read_lines("././data/sample");
+            let lines = super::lines_to_int(file_content);
+            assert_eq!(lines.len(),6);
+            for line in lines {
+                   assert_eq!(line, 1);
+            }
+
+    }
+        
 }
