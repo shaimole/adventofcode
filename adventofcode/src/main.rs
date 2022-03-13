46a2f36740062dab09f13a9770702bc4212a8063
unknown <felixurlaub@googlemail.com>
2022-03-13 10:29:51 +0100

    solution part 2 day 1

--- adventofcode/src/main.rs
+++ adventofcode/src/main.rs
@@ -1,5 +1,5 @@
 use day1;
 
 fn main() {
-    println!("{0}",day1::solve("./././day1/data/sample1"));
+    println!("{0}",day1::solve2("./././day1/data/sample1"));
 }
--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -15,6 +15,22 @@ where P: AsRef<Path>,{
     return rise_counter;
 }
 
+pub fn solve2<P>(filename: P) -> i32 
+where P: AsRef<Path>,{
+    let data = read_data(filename);
+    let mut previous_depth = 0;
+    let mut rise_counter = 0;
+    for i in 3..data.len() {
+        let depth = data[i-2] + data[i-1] + data[i];
+         if depth > previous_depth {
+            rise_counter = rise_counter + 1;
+        }
+        previous_depth = depth;
+    }
+    return rise_counter;
+}
+
+
 fn read_data<P>(filename: P) -> Vec<i32> 
 where P: AsRef<Path>, {
     let lines = util::read_lines(filename);
@@ -30,10 +46,13 @@ mod tests {
         let result = super::read_data("././data/test");
         assert_eq!(result.len(),10);
     }
+
     #[test]
     fn it_should_solve_testdata_for_part_1() {
         assert_eq!(super::solve1("././data/test"),7);
     }
+
+    #[test]    
     fn it_should_solve_testdata_for_part_2() {
         assert_eq!(super::solve2("././data/test"),5);
     }
