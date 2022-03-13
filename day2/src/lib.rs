83a56e93da7d3c3fd01bdac003c1927d36ff5bf9
unknown <felixurlaub@googlemail.com>
2022-03-13 10:33:26 +0100

    base for day2

---
+++ day2/src/lib.rs
@@ -0,0 +1,40 @@
+use util;
+use std::path::Path;
+
+pub fn solve1<P>(filename: P) -> i32 
+where P: AsRef<Path>,{
+    return 1;
+}
+
+pub fn solve2<P>(filename: P) -> i32 
+where P: AsRef<Path>,{
+    return 1;
+}
+
+
+fn read_data<P>(filename: P) -> Vec<i32> 
+where P: AsRef<Path>, {
+    let lines = util::read_lines(filename);
+    return util::lines_to_int(lines);
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_should_read_the_testfile() {
+        let result = super::read_data("././data/test");
+        assert_eq!(result.len(),10);
+    }
+
+    // #[test]
+    // fn it_should_solve_testdata_for_part_1() {
+    //     assert_eq!(super::solve1("././data/test"),7);
+    // }
+
+    // #[test]    
+    // fn it_should_solve_testdata_for_part_2() {
+    //     assert_eq!(super::solve2("././data/test"),5);
+    // }
+}
