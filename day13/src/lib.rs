2b914efe5889f768bc001b7f3a94545f6f20877f
unknown <felixurlaub@googlemail.com>
2022-10-09 18:50:17 +0200

    add skeleton for day13

---
+++ day13/src/lib.rs
@@ -0,0 +1,31 @@
+use common;
+use std::path::Path;
+
+pub fn solve1<P>(filename: P) -> u16 
+ where P: AsRef<Path>,{
+  return 1;
+}
+
+
+
+
+pub fn solve2<P>(filename: P) -> u32 
+where P: AsRef<Path>,{
+    return 1;
+}
+ 
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_should_parse_data_correctly() {
+    }
+
+    fn it_should_solve_part_1() {
+    }
+
+    #[test]
+    fn it_should_solve_part_2() {
+    }
+}
