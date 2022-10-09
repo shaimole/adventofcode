7de46f8b813d8724b2ff115cd0247ef12ac56b0b
unknown <felixurlaub@googlemail.com>
2022-10-09 18:56:14 +0200

    add sample test and sample data

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -19,9 +19,13 @@ where P: AsRef<Path>,{
 mod tests {
 
     #[test]
-    fn it_should_parse_data_correctly() {
+    fn it_should_parse_data_correctly() {   
     }
-
+    
+    #[test]
+    fn it_should_solve_sample() {
+        assert_eq!(super::solve1("././data/sample"),17)
+    }
     fn it_should_solve_part_1() {
     }
 
