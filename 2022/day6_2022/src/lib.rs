b447d9b93f17d3306b5262e9a470cdd9179b76fb
unknown <felixurlaub@googlemail.com>
2022-12-06 22:10:26 +0100

    solve day 6

---
+++ 2022/day6_2022/src/lib.rs
@@ -0,0 +1,54 @@
+
+use std::collections::HashSet;
+
+pub fn solve(datastream: Vec<char>, window_size: usize) -> usize {
+    let datastream_as_slice: &[char] = &datastream[..];
+    for (i, set) in datastream_as_slice.windows(window_size).enumerate() {
+        let as_hash: HashSet<&char> = set.iter().collect();
+        if as_hash.len() == set.len() {
+            return i + window_size;
+        }
+    }
+    unreachable!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_should_solve_sample() {
+        let datastream_1: Vec<char> = common::read_lines("./data/sample")[0].chars().collect();
+        assert_eq!(solve(datastream_1,4),5);
+        let datastream_2: Vec<char> = common::read_lines("./data/sample")[1].chars().collect();
+        assert_eq!(solve(datastream_2,4),6);
+        let datastream_3: Vec<char> = common::read_lines("./data/sample")[2].chars().collect();
+        assert_eq!(solve(datastream_3,4),10);
+        let datastream_4: Vec<char> = common::read_lines("./data/sample")[3].chars().collect();
+        assert_eq!(solve(datastream_4,4),11);
+    }
+
+    #[test]
+    fn it_should_solve_sample_part2() {
+        let datastream_1: Vec<char> = common::read_lines("./data/sample2")[0].chars().collect();
+        assert_eq!(solve(datastream_1,14),19);
+        let datastream_2: Vec<char> = common::read_lines("./data/sample2")[1].chars().collect();
+        assert_eq!(solve(datastream_2,14),23);
+        let datastream_3: Vec<char> = common::read_lines("./data/sample2")[2].chars().collect();
+        assert_eq!(solve(datastream_3,14),23);
+        let datastream_4: Vec<char> = common::read_lines("./data/sample2")[3].chars().collect();
+        assert_eq!(solve(datastream_4,14),29);
+    }
+    #[test]
+    fn it_should_solve_part_1() {
+        let datastream: Vec<char> = common::read_lines("./data/input")[0].chars().collect();
+        assert_eq!(solve(datastream,4),1876)
+    }
+
+    #[test]
+    fn it_should_solve_part_2() {
+        let datastream: Vec<char> = common::read_lines("./data/input")[0].chars().collect();
+        assert_eq!(solve(datastream,14),2202)
+    }
+
+}
