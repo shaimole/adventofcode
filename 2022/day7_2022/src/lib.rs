d329011d9bed2142aac85fe5a1dad04c76c51106
Shaimole <felixurlaub@googlemail.com>
2022-12-07 18:43:46 +0100

    add day 7 skeleton

---
+++ 2022/day7_2022/src/lib.rs
@@ -0,0 +1,26 @@
+use std::collections::HashSet;
+
+pub fn solve(datastream: &[char], sequence_len: usize) -> usize {  
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_should_solve_sample() {
+    }
+
+    #[test]
+    fn it_should_solve_sample_part2() {
+    }
+    #[test]
+    fn it_should_solve_part_1() {
+    }
+
+    #[test]
+    fn it_should_solve_part_2() {
+    }
+
+}
