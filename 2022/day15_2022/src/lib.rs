09dec253a6cde18b3b5dd5b0626d49d884df0de0
unknown <felixurlaub@googlemail.com>
2022-12-21 23:19:04 +0100

    solve day 15 part 2

--- 2022/day15_2022/src/lib.rs
+++ 2022/day15_2022/src/lib.rs
@@ -38,35 +38,36 @@ where
         .iter()
         .map(parse_coords)
         .collect();
-    let mut impossible_positons: HashSet<(i64, i64)> = HashSet::new();
-    for coord in &coords {
-        let distance_to_nearest_becon =
-            i64::abs(coord[2] - coord[0]) + i64::abs(coord[3] - coord[1]);
-        for offset in 0..=distance_to_nearest_becon {
-            let impossible_position_amount = distance_to_nearest_becon - offset;
-            for i in 0..=impossible_position_amount {
-                impossible_positons.insert((coord[0] + i, coord[1] + offset));
-                impossible_positons.insert((coord[0] - i, coord[1] + offset));
-                impossible_positons.insert((coord[0] + i, coord[1] - offset));
-                impossible_positons.insert((coord[0] - i, coord[1] - offset));
+
+    let mut hits = 0;
+    for y in 0..=max_y {
+        let mut x = 0;
+        while x <= max_y {
+            for coord in &coords {
+                let sensor_range = i64::abs(coord[2] - coord[0]) + i64::abs(coord[3] - coord[1]);
+                let current_distance_to_sensor = i64::abs(coord[0] - x) + i64::abs(coord[1] - y);
+                if current_distance_to_sensor > sensor_range {
+                    hits += 1;
+                } else {
+                    let currrent_y_distance_to_sensor = i64::abs(coord[1] - y);
+                    let current_x_distance_to_sensor = coord[0] - x;
+                    let impossible_position_amount = sensor_range - currrent_y_distance_to_sensor;
+                    x += current_x_distance_to_sensor + impossible_position_amount;
+                    // println!("sensor range {:?}, current x distance {:?}, self{:?}, sensor{:?},jumping {:?}", sensor_range, current_x_distance_to_sensor, (coord[0],coord[1]),(x,y) ,current_x_distance_to_sensor + impossible_position_amount);
+                    break;
+                }
             }
+            if hits == coords.len() {
+                return x * 4000000 + y;
+            }
+            hits = 0;
+            x += 1;
         }
     }
-    for i in 0..=max_y {
-        for j in 0..max_y {
-            if impossible_positons.contains(&(i, j)) {
-                continue;
-            }
-            if (i >= 0) && (i <= max_y) && (0 <= j) && (j <= max_y) {
-                return i * 4000000 + j;
-            }
-        }
-        // println!("");
-    }
-    // for pos in impossible_positons {}
 
     unreachable!();
 }
+
 fn parse_coords(line: &String) -> Vec<i64> {
     use regex::Regex;
     let re: Regex = Regex::new(r"-?\d+").unwrap();
@@ -95,11 +96,11 @@ mod tests {
 
     #[test]
     fn it_should_solve_part_1() {
-        assert_eq!(solve("./data/input", 2000000), 1)
+        assert_eq!(solve("./data/input", 2000000), 5716881)
     }
 
     #[test]
     fn it_should_solve_part_2() {
-        assert_eq!(solve2("./data/input", 10), 1)
+        assert_eq!(solve2("./data/input", 4000000), 10852583132904)
     }
 }
