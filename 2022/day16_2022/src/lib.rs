1187d29872ef7e6432f0e8048d223ad066b8830d
unknown <felixurlaub@googlemail.com>
2022-12-26 22:06:10 +0100

    solve day 16

--- 2022/day16_2022/src/lib.rs
+++ 2022/day16_2022/src/lib.rs
@@ -4,10 +4,18 @@ use std::path::Path;
 struct Cave {
     key: String,
     edges: Vec<String>,
-    flow: u16,
+    flow: u32,
 }
 
-pub fn solve<P>(filename: P, y_pos: i64) -> u32
+struct Node {
+    key: String,
+    water: u32,
+    time_remaining: u32,
+    current_rate: u32,
+    vistited: Vec<String>,
+}
+
+pub fn solve<P>(filename: P, start: String) -> u32
 where
     P: AsRef<Path>,
 {
@@ -16,7 +24,47 @@ where
         .map(|line| parse_cave(line))
         .collect();
     let (distances, flow_rates) = create_distance_map(caves);
-    println!("{:?}",  distances);
+    let mut max_water = 0;
+    let mut stack = vec![];
+    let root = Node {
+        key: "AA".to_string(),
+        water: 0,
+        time_remaining: 30,
+        current_rate: 0,
+        vistited: vec![],
+    };
+    stack.push(root);
+    while stack.len() > 0 {
+        let current = stack.pop().unwrap();
+        max_water = std::cmp::max(max_water, current.water);
+        let mut branches = 0;
+        for (next, distance) in distances.get(&current.key).unwrap() {
+            if current.time_remaining < distance + 1 {
+                continue;
+            }
+            if current.vistited.contains(next) {
+                continue;
+            }
+            let mut next_visisted = current.vistited.clone();
+            next_visisted.push(next.clone());
+            let next_on_stack = Node {
+                key: next.to_string(),
+                water: current.water + current.current_rate * (distance + 1),
+                time_remaining: current.time_remaining - (distance + 1),
+                current_rate: flow_rates.get(next).unwrap() + current.current_rate,
+                vistited: next_visisted,
+            };
+            stack.push(next_on_stack);
+            branches += 1;
+        }
+        if branches == 0 {
+            max_water = std::cmp::max(
+                max_water,
+                current.water + current.current_rate * current.time_remaining,
+            );
+        }
+    }
+    max_water
     // let relevant: Vec<&Cave> = caves.iter().filter(|cave| cave.flow != 0).collect();
     // create a hashmap where key = cave key and value = hashmap of distances to all other caves
     // create a hashmap where key = cave flow rate
@@ -34,12 +82,11 @@ where
     // increase time depending on distance + 1 (open valve)
     // record max flow rate
     // return max flow rate
-    0
 }
 
 fn create_distance_map(
     caves: Vec<Cave>,
-) -> (HashMap<String, HashMap<String, u32>>, HashMap<String, u16>) {
+) -> (HashMap<String, HashMap<String, u32>>, HashMap<String, u32>) {
     let mut distances = HashMap::new();
     let mut flow_rates = HashMap::new();
     caves.iter().for_each(|cave| {
@@ -59,14 +106,14 @@ fn create_distance_map(
     });
     caves.iter().for_each(|cave| {
         let flow_rate = flow_rates.get(&cave.key).unwrap();
-        if flow_rate == &0 {
+        if flow_rate == &0 && &cave.key != "AA" {
             distances.remove(&cave.key);
         } else {
             caves.iter().for_each(|other| {
                 let flow_rate = flow_rates.get(&other.key).unwrap();
                 if flow_rate == &0 {
                     let cave_distances = distances.get_mut(&cave.key).unwrap();
-                cave_distances.remove(&other.key);
+                    cave_distances.remove(&other.key);
                 }
             })
         }
@@ -95,14 +142,117 @@ fn get_cave_distance<'a>(
     get_cave_distance(collective_edges, end, distances, distance + 1)
 }
 
-fn solve2<P>(filename: P, max_y: i64) -> i64
+fn solve2<P>(filename: P) -> u32
 where
     P: AsRef<Path>,
 {
-    0
+    use itertools::Itertools;
+    let caves: Vec<Cave> = common::read_lines(filename)
+        .iter()
+        .map(|line| parse_cave(line))
+        .collect();
+    let (distances, flow_rates) = create_distance_map(caves);
+    let mut max_water = 0;
+    for i in 8..=distances.len() / 2 {
+        // educated guess split in 2 xd
+        let mut max_water_comb = 0;
+        distances.iter().combinations(i).for_each(|allowed| {
+            let mut max_water_comb_1 = 0;
+            let mut max_water_comb_2 = 0;
+
+            let mut stack = vec![];
+            let root = Node {
+                key: "AA".to_string(),
+                water: 0,
+                time_remaining: 26,
+                current_rate: 0,
+                vistited: vec![],
+            };
+            stack.push(root);
+            while stack.len() > 0 {
+                let current = stack.pop().unwrap();
+                max_water_comb_1 = std::cmp::max(max_water_comb_1, current.water);
+                let mut branches = 0;
+                for (next, distance) in distances.get(&current.key).unwrap() {
+                    if current.time_remaining < distance + 1 {
+                        continue;
+                    }
+                    if current.vistited.contains(next) {
+                        continue;
+                    }
+                    if allowed.iter().filter(|(k, _)| k == &next).count() == 0 {
+                        continue;
+                    }
+                    let mut next_visisted = current.vistited.clone();
+                    next_visisted.push(next.clone());
+                    let next_on_stack = Node {
+                        key: next.to_string(),
+                        water: current.water + current.current_rate * (distance + 1),
+                        time_remaining: current.time_remaining - (distance + 1),
+                        current_rate: flow_rates.get(next).unwrap() + current.current_rate,
+                        vistited: next_visisted,
+                    };
+                    stack.push(next_on_stack);
+                    branches += 1;
+                }
+                if branches == 0 {
+                    max_water_comb_1 = std::cmp::max(
+                        max_water_comb_1,
+                        current.water + current.current_rate * current.time_remaining,
+                    );
+                }
+            }
+
+            let mut stack = vec![];
+            let root = Node {
+                key: "AA".to_string(),
+                water: 0,
+                time_remaining: 26,
+                current_rate: 0,
+                vistited: vec![],
+            };
+            stack.push(root);
+            while stack.len() > 0 {
+                let current = stack.pop().unwrap();
+                max_water_comb_2 = std::cmp::max(max_water_comb_2, current.water);
+                let mut branches = 0;
+                for (next, distance) in distances.get(&current.key).unwrap() {
+                    if current.time_remaining < distance + 1 {
+                        continue;
+                    }
+                    if current.vistited.contains(next) {
+                        continue;
+                    }
+                    if allowed.iter().filter(|(k, _)| k == &next).count() > 0 {
+                        continue;
+                    }
+                    let mut next_visisted = current.vistited.clone();
+                    next_visisted.push(next.clone());
+                    let next_on_stack = Node {
+                        key: next.to_string(),
+                        water: current.water + current.current_rate * (distance + 1),
+                        time_remaining: current.time_remaining - (distance + 1),
+                        current_rate: flow_rates.get(next).unwrap() + current.current_rate,
+                        vistited: next_visisted,
+                    };
+                    stack.push(next_on_stack);
+                    branches += 1;
+                }
+                if branches == 0 {
+                    max_water_comb_2 = std::cmp::max(
+                        max_water_comb_2,
+                        current.water + current.current_rate * current.time_remaining,
+                    );
+                }
+            }
+            max_water_comb = std::cmp::max(max_water_comb, max_water_comb_1 + max_water_comb_2);
+        });
+        max_water = std::cmp::max(max_water, max_water_comb);
+    }
+    max_water
 }
 
-fn get_flow_rate(line: &String) -> u16 {
+fn get_flow_rate(line: &String) -> u32 {
     use regex::Regex;
     let re: Regex = Regex::new(r"-?\d+").unwrap();
     re.find(line).unwrap().as_str().parse().unwrap()
@@ -153,21 +303,21 @@ mod tests {
 
     #[test]
     fn it_should_solve_sample() {
-        assert_eq!(solve("./data/sample", 10), 26)
+        assert_eq!(solve("./data/sample", "AA".to_string()), 1651)
     }
 
     #[test]
     fn it_should_solve_sample2() {
-        assert_eq!(solve2("./data/sample", 20), 56000011)
+        assert_eq!(solve2("./data/sample"), 1707)
     }
 
     #[test]
     fn it_should_solve_part_1() {
-        assert_eq!(solve("./data/input", 2000000), 5716881)
+        assert_eq!(solve("./data/input", "TM".to_string()), 5716881)
     }
 
     #[test]
     fn it_should_solve_part_2() {
-        assert_eq!(solve2("./data/input", 4000000), 10852583132904)
+        assert_eq!(solve2("./data/input"), 1707)
     }
 }
