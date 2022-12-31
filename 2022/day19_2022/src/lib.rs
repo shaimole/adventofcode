040dfe7b64a76a1c5eb2cbda6149a781fb03c2f0
Shaimole <felixurlaub@googlemail.com>
2022-12-31 16:19:42 +0100

    add blueprint parser

--- 2022/day19_2022/src/lib.rs
+++ 2022/day19_2022/src/lib.rs
@@ -1,6 +1,14 @@
 use std::collections::HashSet;
 use std::path::Path;
 
+struct Blueprint {
+    costs: Vec<Vec<u32>>,
+}
+struct State {
+    income: Vec<u32>,
+    amount: Vec<u32>,
+    time: i32,
+}
 pub fn solve<P>(filename: P) -> u32
 where
     P: AsRef<Path>,
@@ -15,7 +23,33 @@ where
     0
 }
 
+fn parse<P>(filename: P) -> Vec<Blueprint>
+where
+    P: AsRef<Path>,
+{
+    common::read_lines(filename)
+        .iter()
+        .map(|line| {
+            let costs = extract_costs(line);
+            Blueprint {
+                costs: vec![
+                    vec![costs[1], 0, 0, 0],
+                    vec![costs[2], 0, 0, 0],
+                    vec![costs[3], costs[4], 0, 0],
+                    vec![costs[5], costs[6], costs[7], 0],
+                ],
+            }
+        })
+        .collect()
+}
 
+fn extract_costs(line: &String) -> Vec<u32> {
+    use regex::Regex;
+    let re: Regex = Regex::new(r"\d").unwrap();
+    re.find_iter(line)
+        .map(|cost| cost.as_str().parse().unwrap())
+        .collect()
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -25,6 +59,18 @@ mod tests {
         assert_eq!(solve("./data/sample"), 33)
     }
 
+    #[test]
+    fn it_should_parse_blueprint() {
+        let expected = Blueprint {
+            costs: vec![
+                vec![4, 0, 0, 0],
+                vec![2, 0, 0, 0],
+                vec![3, 1, 0, 0],
+                vec![4, 2, 7, 0],
+            ],
+        };
+        assert_eq!(parse("./data/poc")[0].costs, expected.costs);
+    }
     #[test]
     fn it_should_solve_poc() {
         assert_eq!(solve("./data/poc"), 9)
@@ -41,6 +87,6 @@ mod tests {
 
     #[test]
     fn it_should_solve_part_2() {
-        assert_eq!(solve2("./data/input"),1)
+        assert_eq!(solve2("./data/input"), 1)
     }
 }
