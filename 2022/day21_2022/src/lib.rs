15dc82b77594f4e4ae6990a9789c05347658a11c
Shaimole <felixurlaub@googlemail.com>
2023-01-02 20:27:06 +0100

    add progress for part2

--- 2022/day21_2022/src/lib.rs
+++ 2022/day21_2022/src/lib.rs
@@ -46,13 +46,13 @@ where
                 let a: i128 = *known.get(&monkey[1]).unwrap();
                 let b: i128 = *known.get(&monkey[2]).unwrap();
                 let monkey_nr = match monkey[3].as_str() {
-                    "*" => a*b,
-                    "+" => a+b,
-                    "-" => a-b,
-                    "/" => a/b,
+                    "*" => a * b,
+                    "+" => a + b,
+                    "-" => a - b,
+                    "/" => a / b,
                     _ => unreachable!(),
                 };
-                known.insert(monkey[0].clone(),monkey_nr);
+                known.insert(monkey[0].clone(), monkey_nr);
                 unknown.remove(i);
                 break;
             }
@@ -66,7 +66,79 @@ pub fn solve2<P>(filename: P) -> i128
 where
     P: AsRef<Path>,
 {
-    0
+    let (mut known, mut unknown) = parse(filename);
+    known.remove(&("humn".to_string()));
+    let mut prev_ukn = 0;
+    while prev_ukn != unknown.len() {
+        prev_ukn = unknown.len();
+        for i in 0..unknown.len() {
+            let monkey = unknown[i].clone();
+
+            if known.contains_key(&monkey[1]) && known.contains_key(&monkey[2]) {
+                let a: i128 = *known.get(&monkey[1]).unwrap();
+                let b: i128 = *known.get(&monkey[2]).unwrap();
+                let monkey_nr = match monkey[3].as_str() {
+                    "*" => a * b,
+                    "+" => a + b,
+                    "-" => a - b,
+                    "/" => a / b,
+                    _ => unreachable!(),
+                };
+                known.insert(monkey[0].clone(), monkey_nr);
+                unknown.remove(i);
+                break;
+            }
+        }
+    }
+
+    println!("{:?}", known);
+    let unknown_map: HashMap<String, (String, String, String)> = unknown
+        .iter()
+        .map(|v| (v[0].clone(), (v[1].clone(), v[2].clone(), v[3].clone())))
+        .collect();
+    let root = unknown_map.get(&("root".to_string())).unwrap();
+
+    if known.contains_key(&root.1) {
+        known.insert(root.0.clone(), *known.get(&root.1).unwrap());
+    } else {
+        known.insert(root.1.clone(), *known.get(&root.0).unwrap());
+    }
+    println!("{:?}", known);
+    while !known.contains_key(&"humn".to_string()) {
+        for (key, unknown) in unknown_map.iter() {
+            if !known.contains_key(key) {
+                continue;
+            }
+
+            if known.contains_key(&unknown.0) {
+                let a: i128 = *known.get(key).unwrap();
+                let b: i128 = *known.get(&unknown.0).unwrap();
+                let monkey_nr = match unknown.2.as_str() {
+                    "*" => a / b,
+                    "+" => a - b,
+                    "-" => a + b,
+                    "/" => a * b,
+                    _ => unreachable!(),
+                };
+                known.insert(unknown.1.clone(), monkey_nr);
+            }else 
+            if known.contains_key(&unknown.1) {
+                let a: i128 = *known.get(key).unwrap();
+                let b: i128 = *known.get(&unknown.1).unwrap();
+                
+                let monkey_nr = match unknown.2.as_str() {
+                    "*" => a / b,
+                    "+" => a - b,
+                    "-" => a + b,
+                    "/" => a * b,
+                    _ => unreachable!(),
+                };
+                known.insert(unknown.0.clone(), monkey_nr);
+            println!("{:?}", (a,b,monkey_nr));
+            }
+        }
+    }
+    *known.get(&"humn".to_string()).unwrap()
 }
 
 #[cfg(test)]
@@ -81,11 +153,11 @@ mod tests {
 
     #[test]
     fn it_should_solve_sample_part2() {
-        assert_eq!(solve2("./data/sample"), -1)
+        assert_eq!(solve2("./data/sample"), 301)
     }
     #[test]
     fn it_should_solve_part_1() {
-        assert_eq!(solve("./data/input"), -1)
+        assert_eq!(solve("./data/input"), 22382838633806)
     }
 
     #[test]
