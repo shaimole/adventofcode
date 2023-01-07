1e27b23f8ca65210cb102989cfc9e8345bfc5e90
Shaimole <felixurlaub@googlemail.com>
2023-01-07 14:35:39 +0100

    solve part 2

--- 2022/day23_2022/src/lib.rs
+++ 2022/day23_2022/src/lib.rs
@@ -141,7 +141,90 @@ fn solve2<P>(filename: P) -> u32
 where
     P: AsRef<Path>,
 {
-    1
+    let mut elfs = parse(filename);
+    let (mut max_x, mut max_y, mut min_x, mut min_y) = (i64::MIN, i64::MIN, i64::MAX, i64::MAX);
+
+    for i in 0..1000 {
+        let mut moves = HashMap::new();
+
+        for elf in elfs.clone() {
+            let mut move_to = elf;
+            let dont_move = ADJECENT
+                .iter()
+                .map(|direction| (elf.0 + direction.0, elf.1 + direction.1))
+                .filter(|target| elfs.contains(&target))
+                .count()
+                == 0;
+            if dont_move {
+                continue;
+            }
+            for c in 0..4 {
+                let dir = (i + c) % 4;
+                // println!(" checking direction {dir} for elf {:?}", elf);
+
+                let direction_is_empty = CONSIDER[dir]
+                    .iter()
+                    .map(|direction| (elf.0 + direction.0, elf.1 + direction.1))
+                    .filter(|target| elfs.contains(&target))
+                    .count()
+                    == 0;
+                // println!(
+                //     " for elf : {:?}, direction {dir} is empty {direction_is_empty}",
+                //     elf
+                // );
+
+                if direction_is_empty {
+                    match dir {
+                        0 => match moves.get(&(elf.0, elf.1 + 1)) {
+                            Some((e, count)) => {
+                                moves.insert((elf.0, elf.1 + 1), (elf, count + 1));
+                            }
+                            None => {
+                                moves.insert((elf.0, elf.1 + 1), (elf, 1));
+                            }
+                        },
+                        1 => match moves.get(&(elf.0, elf.1 - 1)) {
+                            Some((e, count)) => {
+                                moves.insert((elf.0, elf.1 - 1), (elf, count + 1));
+                            }
+                            None => {
+                                moves.insert((elf.0, elf.1 - 1), (elf, 1));
+                            }
+                        },
+                        3 => match moves.get(&(elf.0 + 1, elf.1)) {
+                            Some((_, count)) => {
+                                moves.insert((elf.0 + 1, elf.1), (elf, count + 1));
+                            }
+                            None => {
+                                moves.insert((elf.0 + 1, elf.1), (elf, 1));
+                            }
+                        },
+                        _ => match moves.get(&(elf.0 - 1, elf.1)) {
+                            Some((_, count)) => {
+                                moves.insert((elf.0 - 1, elf.1), (elf, count + 1));
+                            }
+                            None => {
+                                moves.insert((elf.0 - 1, elf.1), (elf, 1));
+                            }
+                        },
+                    }
+                    break;
+                }
+            }
+        }
+        if moves.iter().filter(|(target_pos, (elf, count))| 
+            count == &1).count() == 0 {
+            return i as u32 +1;
+        }
+        moves.iter().for_each(|(target_pos, (elf, count))| {
+            if count == &1 {
+                elfs.remove(elf);
+                elfs.insert(*target_pos);
+            }
+        });
+
+    }
+    unreachable!();
 }
 
 fn parse<P>(filename: P) -> HashSet<(i64, i64)>
@@ -177,11 +260,11 @@ mod tests {
 
     #[test]
     fn it_should_solve_part_1() {
-        assert_eq!(solve("./data/input"), 0)
+        assert_eq!(solve("./data/input"), 4162)
     }
 
     #[test]
     fn it_should_solve_part_2() {
-        assert_eq!(solve2("./data/input"), 0)
+        assert_eq!(solve2("./data/input"), 986)
     }
 }
